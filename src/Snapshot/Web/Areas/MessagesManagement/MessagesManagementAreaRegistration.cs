db0aaad17e955db1d42da00427af5724765701dd
Claudia Glodan <claudia.glodan@evozon.com>
2012-05-09 16:23:23 +0300

    Messages / Drugstore - Layout

---
+++ src/Snapshot/Web/Areas/MessagesManagement/MessagesManagementAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.MessagesManagement
+{
+    public class MessagesManagementAreaRegistration : AreaRegistration
+    {
+        public const string DEFAULT_ROUTE = "MessagesManagement_default";
+        public override string AreaName
+        {
+            get
+            {
+                return "MessagesManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                DEFAULT_ROUTE,
+                "MessagesManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
