7657db9ea2ec15b64535f9e228e97c5aec5bd7ba
Claudia Glodan <claudia.glodan@evozon.com>
2012-06-07 14:52:38 +0300

    Google Map Report - Map Pannel

---
+++ src/Snapshot/Web/Areas/AnalysisManagement/Controllers/GoogleMapReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Areas.AnalysisManagement.Controllers
+{
+    public class GoogleMapReportController : Controller
+    {
+        public ActionResult Overview()
+        {
+            return View();
+        }
+
+    }
+}
