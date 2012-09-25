3e89d2167f287b6fb4b0ba7f8f332e830a59a429
Claudia Glodan <claudia.glodan@evozon.com>
2012-09-25 09:12:09 +0300

    Priority 2: Advice Report - Keyword in chart

--- src/Snapshot/Web/Areas/AnalysisManagement/Controllers/AdviceReportController.cs
+++ src/Snapshot/Web/Areas/AnalysisManagement/Controllers/AdviceReportController.cs
@@ -59,7 +59,7 @@ namespace Web.Areas.AnalysisManagement.Controllers
                 {
                     AdviceReportModel model = new AdviceReportModel();
                     model.Advice =  advice.Code;
-                    model.Outpost = dispensary.Name;
+                    model.Outpost = dispensary.Name + " (" + dispensary.District.Name + ")";
 
                     PatientsModel patients = GetNumberOfPatients(dispensary, inputModel.startDate, inputModel.endDate, advice);
                     model.Female = patients.Female;
@@ -143,7 +143,7 @@ namespace Web.Areas.AnalysisManagement.Controllers
                 }
 
                 AdviceReportModel model = new AdviceReportModel();
-                model.Advice = advice.Code;
+                model.Advice = advice.Code + " (" + advice.Keyword + ")";
                 model.Female = female;
                 model.Male = male;
                 model.NumberOfPatients = total;
